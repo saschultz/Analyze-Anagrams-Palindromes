c06166829d22b4377a059291f4e312da4006312f
Sara Schultz <saschultz95@gmail.com>
2017-04-21 12:20:27 -0700

    pass second spec to test if words are anagrams regardless of case.

--- spec/analyze_spec.rb
+++ spec/analyze_spec.rb
@@ -8,4 +8,8 @@ describe 'analyze#analyze_anagram' do
     expect(input_words.analyze_anagram('ruby', 'bury')).to(eq('These words are anagrams.'))
   end
 
+  it('checks if two words are anagrams, regardless of case') do
+    expect(input_words.analyze_anagram('Eat', 'TEa')).to(eq('These words are anagrams.'))
+  end
+
 end
